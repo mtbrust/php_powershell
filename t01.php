ed4b5abe91e117c9dfc6112b96112e4ca1f23057
Mateus Brust <mtbrust@gmail.com>
2021-03-31 23:29:12 -0300

    arquivo para integracao

--- t01.php
+++ t01.php
@@ -13,12 +13,20 @@
 
 
 header('Content-Type: text/html; charset=utf-8');
+
+if($_POST){
+  $user = $_POST['user'];
+  $msg = $_POST['msg'];
+  shell_exec("MSG $user $msg");
+}
+
 $output = '';
 // $output = shell_exec('ipconfig'); //OK
 // $output = shell_exec('MSG /SERVER:127.0.0.1 /TIME:05 * HELLO WORLD'); //OK
 // $output = shell_exec('MSG /SERVER:DESV-01 * Mensagem corrida! chão é'); //OK
+// $output = shell_exec('MSG brust * Mensagem corrida! chão é'); //OK
 // $output = shell_exec('time'); //OK
-//$output = shell_exec('msg /server192168.1.3 desv-01 "ola"');
+// $output = shell_exec('msg /server192168.1.3 desv-01 "ola"'); // ERRO
 echo "<pre>$output</pre>";
 // echo Shell_Exec ('powershell.exe -executionpolicy bypass -NoProfile -Command "Get-Process | ConvertTo-Html"'); // Demora um pouco.
 
@@ -33,8 +41,21 @@ date_default_timezone_set('America/Sao_Paulo');
 
 ini_set('html_errors', '1');
 
-var_dump($_SERVER);
+//var_dump($_SERVER);
 
 echo '<hr>';
 
-printf($_SERVER['REMOTE_USER'], $_SERVER['PHP_AUTH_USER']);
+printf($_SERVER['REMOTE_USER']);
+
+echo '<hr>';
+echo $_SERVER['PHP_AUTH_USER'];
+
+?>
+
+<form action="" method="POST">
+<input name="user" type="text" placeholder="user" value="brust">
+<br>
+<textarea name="msg" id="" cols="30" rows="10"></textarea>
+<br>
+<button type="submit">Enviar</button>
+</form>
